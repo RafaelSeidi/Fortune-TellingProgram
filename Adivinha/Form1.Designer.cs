52ac35fcc798d29b7e4fc120a3e339194b0c9a55
Rafael Seidi Suzuki Tomita <100170190+RafaelSeidi@users.noreply.github.com>
2022-03-14 12:18:07 -0300

    Add files via upload

---
+++ Adivinha/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Adivinha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 229);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.FloralWhite;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(0, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Próximo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 304);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Adivinha/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adivinha
+{
+    public partial class Form1 : Form
+    {
+        int quadro = 1;
+        int adv = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "Quadro " + quadro;
+
+            switch (quadro)
+            {
+                case 1:
+                    pictureBox1.ImageLocation = "figura1.jpg";
+                    if (MessageBox.Show("Seu número está neste quadro?", "Quadro 1", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        adv += 1;
+                    }
+                    break;
+
+                case 2:
+                    pictureBox1.ImageLocation = "figura2.jpg";
+                    if (MessageBox.Show("Seu número está neste quadro?", "Quadro 2", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        adv += 2;
+                    }
+                    break;
+
+                case 3:
+                    pictureBox1.ImageLocation = "figura3.jpg";
+                    if (MessageBox.Show("Seu número está neste quadro?", "Quadro 3", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        adv += 4;
+                    }
+                    break;
+
+                case 4:
+                    pictureBox1.ImageLocation = "figura4.jpg";
+                    if (MessageBox.Show("Seu número está neste quadro?", "Quadro 4", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        adv += 8;
+                    }
+                    break;
+
+                case 5:
+                    pictureBox1.ImageLocation = "figura5.jpg";
+                    if (MessageBox.Show("Seu número está neste quadro?", "Quadro 5", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        adv += 16;
+                    }
+                    break;
+
+                case 6:
+                    pictureBox1.ImageLocation = "figura6.jpg";
+                    if (MessageBox.Show("Seu número está neste quadro?", "Quadro 6", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        adv += 32;
+                    }
+                    break;
+
+                case 7:
+                    MessageBox.Show("O número que você pensou é o " + adv + "!", "ADIVINHEI");
+                    break;
+            }
+            quadro += 1;
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
